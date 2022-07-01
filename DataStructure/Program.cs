96814d276250376317c8369a30572f8104c3b5b2
Krista <knderson21@gmail.com>
2022-07-01 14:12:38 -0400

    finished data structure lab

---
+++ DataStructure/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Please enter a word you would like to reverse:");
+string wordChoice = Console.ReadLine();
+
+
+
+Console.WriteLine("\nYour word in reverse is: " + ReverseString(wordChoice));
+
+
+
+
+
+
+//Method
+
+static string ReverseString(string wordChoice)
+{
+
+    char[] stringArray = wordChoice.ToCharArray();
+
+    string reverse = String.Empty;
+
+    for (int i = stringArray.Length - 1; i >= 0; i--)
+    {
+        reverse += stringArray[i];
+    }
+
+    return reverse;
+
+}
